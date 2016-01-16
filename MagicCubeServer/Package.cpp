854d66e807afdf0bf65b4d95e851d3c4171e42bc
Wende Tan <twd2@163.com>
2016-01-16 15:37:27 +0800

    suger fix & bugfix & server.timer

--- MagicCubeServer/Package.cpp
+++ MagicCubeServer/Package.cpp
@@ -6,3 +6,8 @@ void *Package::operator new(size_t size, package_len_t dataLen)
 	size_t dataSize = static_cast<size_t>(dataLen);
 	return ::operator new(sizeof(PackageHeader) + dataSize);
 }
+
+void Package::operator delete(void *ptr)
+{
+	::operator delete(ptr);
+}
